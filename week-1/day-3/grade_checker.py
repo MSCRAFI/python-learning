c1317a5d44878e0abb6525cadadc0321e7bbced1
SC Rafi <mscrafi.pro@gmail.com>
2025-09-09 04:41:54 +0600

    Day 3 of Learning Python

---
+++ week-1/day-3/grade_checker.py
@@ -0,0 +1,11 @@
+marks = input("Enter your marks: ")
+if int(marks) >= 90:
+    print("Your grade is A")
+elif int(marks) >= 80:
+    print("Your grade is B")
+elif int(marks) >= 70:
+    print("Your grade is C")
+elif int(marks) >= 60:
+    print("Your grade is D")
+else:
+    print("Your grade is F")
---
+++ week-1/day-3/practice.py
@@ -0,0 +1,7 @@
+temperature = float(input("Enter the temperature in Celsius: "))
+if temperature > 30:
+    print("It's a hot day")
+elif temperature < 10:
+    print("It's a cold day")
+else:
+    print("It's a lovely day")
