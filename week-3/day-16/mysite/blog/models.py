04f48e056edb28c88fbfefd91c5545bc5fcf879d
SC Rafi <mscrafi.pro@gmail.com>
2025-09-21 13:34:16 +0600

    Add Post model and register it in admin

--- week-3/day-16/mysite/blog/models.py
+++ week-3/day-16/mysite/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200) # Title of the blog post
+    content = models.TextField() # Content of the blog post
+    created_at = models.DateTimeField(auto_now_add=True) # Date and time of creation
+    updated_at = models.DateTimeField(auto_now=True) # Date and time of last update
+
+    def __str__(self):
+        return self.title
