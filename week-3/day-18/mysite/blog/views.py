141554080e858eda283b996bcd41c9c407a00945
SC Rafi <mscrafi.pro@gmail.com>
2025-09-23 15:28:43 +0600

    Implement blog homepage with dynamic post listing, update styles, and configure URLs

--- week-3/day-18/mysite/blog/views.py
+++ week-3/day-18/mysite/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def home(request):
+    posts = Post.objects.all().order_by('-created_at')  # Fetch all posts ordered by creation date
+    return render(request, 'blog/home.html', {'posts': posts})  # Render the home template with posts context
