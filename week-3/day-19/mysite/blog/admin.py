e2ec554db8945a0122cadb57640c69de323ad178
SC Rafi <mscrafi.pro@gmail.com>
2025-09-24 16:10:33 +0600

    Add blog app with Post model, admin configuration, and initial migrations

---
+++ week-3/day-19/mysite/blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at') # Fields to display in the admin list view
+    list_filter = ('created_at', 'updated_at') # Filters for the admin list view
+    search_fields = ('title', 'content') # Searchable fields in the admin list view
+    readonly_fields = ('created_at', 'updated_at') # Fields that are read-only in the admin detail view
