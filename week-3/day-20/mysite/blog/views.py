e9c99dd95dd9a2e640e34d6d460080bdaad7aa3e
SC Rafi <mscrafi.pro@gmail.com>
2025-09-26 16:48:24 +0600

    Implement CRUD functionality for blog posts with updated views and templates

--- week-3/day-20/mysite/blog/views.py
+++ week-3/day-20/mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_list_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from .forms import PostForm
 
@@ -11,8 +11,40 @@ def post_create(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save()
-            return redirect(post.get_absolute_url())
+            return redirect('post_list')
     else:
         form = PostForm
 
     return render(request, 'blog/post_form.html', {'form': form})
+
+# List
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+# Detail
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+# UPDATE
+def post_update(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect("post_detail", pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, "blog/post_form.html", {"form": form})
+
+# DELETE
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.delete()
+        return redirect("post_list")
+    return render(request, "blog/post_confirm_delete.html", {"post": post})
